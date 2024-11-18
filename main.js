b44acf246ff002539e0f6227d4b27034c642a023
Poptale <pratik04jha@gmail.com>
2024-11-18 12:52:43 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,131 @@
+let dot = document.querySelector(".dot");
+let block = document.querySelector(".block");
+let block2 = document.querySelector(".block2");
+let block3 = document.querySelector(".block3");
+let block4 = document.querySelector(".block4");
+let block5 = document.querySelector(".block5");
+let block6 = document.querySelector(".block6");
+let block7 = document.querySelector(".block7");
+let block8 = document.querySelector(".block8");
+let block9 = document.querySelector(".block9");
+let block10 = document.querySelector(".block10");
+let block11 = document.querySelector(".block11");
+let block12 = document.querySelector(".block12");
+let block13 = document.querySelector(".block13");
+let block14 = document.querySelector(".block14");
+let block15 = document.querySelector(".block15");
+let block16 = document.querySelector(".block16");
+let block17 = document.querySelector(".block17");
+let block18 = document.querySelector(".block18");
+let block19 = document.querySelector(".block19");
+let block20 = document.querySelector(".block20");
+let block21 = document.querySelector(".block21");
+let block22 = document.querySelector(".block22");
+let block23 = document.querySelector(".block23");
+let block24 = document.querySelector(".block24");
+let block25 = document.querySelector(".block25");
+let block26 = document.querySelector(".block26");
+let block27 = document.querySelector(".block27");
+let block28 = document.querySelector(".block28");
+let block29 = document.querySelector(".block29");
+let block30 = document.querySelector(".block30");
+let block31 = document.querySelector(".block31");
+let block32 = document.querySelector(".block32");
+let block33 = document.querySelector(".block33");
+let block34 = document.querySelector(".block34");
+let block35 = document.querySelector(".block35");
+let block36 = document.querySelector(".block36");
+let block37 = document.querySelector(".block37");
+let block38 = document.querySelector(".block38");
+let block39 = document.querySelector(".block39");
+let block40 = document.querySelector(".block40");
+let block41 = document.querySelector(".block41");
+let block42 = document.querySelector(".block42");
+let block43 = document.querySelector(".block43");
+let block44 = document.querySelector(".block44");
+let block45 = document.querySelector(".block45");
+let block46 = document.querySelector(".block46");
+let block47 = document.querySelector(".block47");
+let block48 = document.querySelector(".block48");
+let block49 = document.querySelector(".block49");
+let block50 = document.querySelector(".block50");
+
+window.addEventListener("mousemove", (e)=> {
+    let posX = e.clientX;
+    let posY = e.clientY;
+    dot.style.left = `${posX}px`;
+    dot.style.top = `${posY}px`;
+    block.animate({left: `${posX}px`, top: `${posY}px`},{duration: 70, fill: "forwards"});
+    block2.animate({left: `${posX}px`, top: `${posY}px`},{duration: 80, fill: "forwards"});
+    block3.animate({left: `${posX}px`, top: `${posY}px`},{duration: 90, fill: "forwards"});
+    block4.animate({left: `${posX}px`, top: `${posY}px`},{duration: 100, fill: "forwards"});
+    block5.animate({left: `${posX}px`, top: `${posY}px`},{duration: 110, fill: "forwards"});
+    block6.animate({left: `${posX}px`, top: `${posY}px`},{duration: 120, fill: "forwards"});
+    block7.animate({left: `${posX}px`, top: `${posY}px`},{duration: 130, fill: "forwards"});
+    block8.animate({left: `${posX}px`, top: `${posY}px`},{duration: 140, fill: "forwards"});
+    block9.animate({left: `${posX}px`, top: `${posY}px`},{duration: 150, fill: "forwards"});
+    block10.animate({left: `${posX}px`, top: `${posY}px`},{duration: 160, fill: "forwards"});
+    block11.animate({left: `${posX}px`, top: `${posY}px`},{duration: 170, fill: "forwards"});
+    block12.animate({left: `${posX}px`, top: `${posY}px`},{duration: 180, fill: "forwards"});
+    block13.animate({left: `${posX}px`, top: `${posY}px`},{duration: 190, fill: "forwards"});
+    block14.animate({left: `${posX}px`, top: `${posY}px`},{duration: 200, fill: "forwards"});
+    block15.animate({left: `${posX}px`, top: `${posY}px`},{duration: 210, fill: "forwards"});
+    block16.animate({left: `${posX}px`, top: `${posY}px`},{duration: 220, fill: "forwards"});
+    block17.animate({left: `${posX}px`, top: `${posY}px`},{duration: 230, fill: "forwards"});
+    block18.animate({left: `${posX}px`, top: `${posY}px`},{duration: 240, fill: "forwards"});
+    block19.animate({left: `${posX}px`, top: `${posY}px`},{duration: 250, fill: "forwards"});
+    block20.animate({left: `${posX}px`, top: `${posY}px`},{duration: 260, fill: "forwards"});
+    block21.animate({left: `${posX}px`, top: `${posY}px`},{duration: 270, fill: "forwards"});
+    block22.animate({left: `${posX}px`, top: `${posY}px`},{duration: 280, fill: "forwards"});
+    block23.animate({left: `${posX}px`, top: `${posY}px`},{duration: 290, fill: "forwards"});
+    block24.animate({left: `${posX}px`, top: `${posY}px`},{duration: 300, fill: "forwards"});
+    block25.animate({left: `${posX}px`, top: `${posY}px`},{duration: 310, fill: "forwards"});
+    block26.animate({left: `${posX}px`, top: `${posY}px`},{duration: 320, fill: "forwards"});
+    block27.animate({left: `${posX}px`, top: `${posY}px`},{duration: 330, fill: "forwards"});
+    block28.animate({left: `${posX}px`, top: `${posY}px`},{duration: 340, fill: "forwards"});
+    block29.animate({left: `${posX}px`, top: `${posY}px`},{duration: 350, fill: "forwards"});
+    block30.animate({left: `${posX}px`, top: `${posY}px`},{duration: 360, fill: "forwards"});
+    block31.animate({left: `${posX}px`, top: `${posY}px`},{duration: 370, fill: "forwards"});
+    block32.animate({left: `${posX}px`, top: `${posY}px`},{duration: 380, fill: "forwards"});
+    block33.animate({left: `${posX}px`, top: `${posY}px`},{duration: 390, fill: "forwards"});
+    block34.animate({left: `${posX}px`, top: `${posY}px`},{duration: 400, fill: "forwards"});
+    block35.animate({left: `${posX}px`, top: `${posY}px`},{duration: 410, fill: "forwards"});
+    block36.animate({left: `${posX}px`, top: `${posY}px`},{duration: 420, fill: "forwards"});
+    block37.animate({left: `${posX}px`, top: `${posY}px`},{duration: 430, fill: "forwards"});
+    block38.animate({left: `${posX}px`, top: `${posY}px`},{duration: 440, fill: "forwards"});
+    block39.animate({left: `${posX}px`, top: `${posY}px`},{duration: 450, fill: "forwards"});
+    block40.animate({left: `${posX}px`, top: `${posY}px`},{duration: 460, fill: "forwards"});
+    block41.animate({left: `${posX}px`, top: `${posY}px`},{duration: 470, fill: "forwards"});
+    block42.animate({left: `${posX}px`, top: `${posY}px`},{duration: 480, fill: "forwards"});
+    block43.animate({left: `${posX}px`, top: `${posY}px`},{duration: 490, fill: "forwards"});
+    block44.animate({left: `${posX}px`, top: `${posY}px`},{duration: 500, fill: "forwards"});
+    block45.animate({left: `${posX}px`, top: `${posY}px`},{duration: 510, fill: "forwards"});
+    block46.animate({left: `${posX}px`, top: `${posY}px`},{duration: 520, fill: "forwards"});
+    block47.animate({left: `${posX}px`, top: `${posY}px`},{duration: 530, fill: "forwards"});
+    block48.animate({left: `${posX}px`, top: `${posY}px`},{duration: 540, fill: "forwards"});
+    block49.animate({left: `${posX}px`, top: `${posY}px`},{duration: 550, fill: "forwards"});
+    block50.animate({left: `${posX}px`, top: `${posY}px`},{duration: 560, fill: "forwards"});
+
+});
+
+
+
+const wrapper = document.querySelector(".main2");
+const yesBtn = document.querySelector(".yesBtn");
+const noBtn = document.querySelector(".noBtn");
+const question = document.querySelector(".question");
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+
+noBtn.addEventListener("mouseover", ()=>{
+    let i = Math.floor(Math.random()*(wrapperRect.width - noBtnRect.width) +1);
+    let j = Math.floor(Math.random()*(wrapperRect.height - noBtnRect.height) +1);
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px'
+})
+noBtn.addEventListener("click", ()=>{
+    question.innerHTML = "💔):How you clicked that :(💔";
+})
+yesBtn.addEventListener("click", ()=>{
+    question.innerHTML = "❤️(; I love you too :)❤️";
+})
